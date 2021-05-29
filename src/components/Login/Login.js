6205c62bb78ba57b0c8b43d6993c6c238814efc4
Abid Nuri <mdabidnuri@gmail.com>
2021-05-29 19:55:35 +0600

    login and responsive done by abid

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './Login.css';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { Button } from '@material-ui/core';
-import {auth,provider} from "./firebase.config"
+import { auth, provider } from "./firebase.config"
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -14,24 +13,27 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(2),
         textAlign: 'center',
         color: theme.palette.text.secondary,
+        align: 'center',
     },
 }));
 
 const Login = () => {
-
-    const signin = () =>{
-        auth.signInWithPopup(provider).catch((error)=> alert (error.message));
+    const classes = useStyles();
+    const signin = () => {
+        auth.signInWithPopup(provider).catch((error) => alert(error.message));
     }
 
     return (
-        <div className="login">
+        <div justify="center" className={classes.root}>
             <Grid container spacing={3}>
-                <Grid item xs={6}>
-                    <img src="https://i.ibb.co/DW6bw09/chat.png" alt="" />
+                <Grid item md={6}>
+                    <img width="100%" src="https://i.ibb.co/DW6bw09/chat.png" alt="" />
                 </Grid>
-                <Grid item xs={6}>
-                    <h2 >Developers Chat</h2>
-                    <Button onClick={signin} variant="contained" color="secondary">Login</Button>
+                <Grid item md={6}>
+                    <div className="login-chat" style={{ paddingTop: "30%" }}>
+                        <h1>Developers Chat</h1>
+                        <Button onClick={signin} variant="contained" color="secondary">Login Here</Button>
+                    </div>
                 </Grid>
             </Grid>
         </div>
