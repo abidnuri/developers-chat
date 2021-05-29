2ecccd1a8b89dcef83fa2935ffa05b21259bb27f
MD Rezaul Karim <rkrezaulkarim07@gmail.com>
2021-05-29 23:22:54 +0600

    logout out

--- src/components/SidebarChat/SidebarChat.js
+++ src/components/SidebarChat/SidebarChat.js
@@ -2,7 +2,7 @@ import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './SidebarChat.css'
 
-const SidebarChat = () => {
+const SidebarChat = ({id, chatName}) => {
     return (
         <div className="sidebarChat">
             <Avatar/>
