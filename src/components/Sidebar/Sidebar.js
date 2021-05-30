3c8316f277e6dfa0133ef226bb1c384c6555c26c
Abid Nuri <mdabidnuri@gmail.com>
2021-05-30 09:55:19 +0600

    abid done

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,15 +1,19 @@
 import { Avatar, IconButton } from '@material-ui/core';
 import { RateReviewOutlined, Search } from '@material-ui/icons';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/user/userSlice';
 import SidebarChat from '../SidebarChat/SidebarChat';
 import './Sidebar.css';
 
 const Sidebar = () => {
+  const user = useSelector(selectUser);
+
     return (
         <div className='sidebar'>
             <h2>I am sidebar</h2>
             <div className="sidebar_header">
-              <Avatar className="sidebar_avatar"/>
+              <Avatar src={user.photo} className="sidebar_avatar"/>
               <div className="sidebar_input">
                  <Search/>
                  <input placeholder='Search'></input>
